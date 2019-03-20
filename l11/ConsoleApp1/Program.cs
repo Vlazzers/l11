5690d51842862896e42b837b4ef4b01f41f05268
juziem <lus0800@mail.ru>
2019-03-20 08:38:25 +0700

    Second

--- l11/ConsoleApp1/Program.cs
+++ l11/ConsoleApp1/Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
             Console.WriteLine("Goodbye World");
+
             Console.ReadKey();
         }
 
